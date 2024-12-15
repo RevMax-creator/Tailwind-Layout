aa5880669c3db278137e55143239f13aa016057c
RevMax-creator <zettabyte@tuta.io>
2024-12-16 01:34:48 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        "sigma-img": "url(https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw)"
+      }
+    },
+  },
+  plugins: [],
+}
+
